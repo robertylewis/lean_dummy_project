4fab2ce8108f531c65b5f9e71c9a5518360d9f70
jrr6 <7482866+jrr6@users.noreply.github.com>
2023-08-28 19:50:40 -0400

    Back to new format

--- LoVe/dummy.lean
+++ LoVe/dummy.lean
@@ -3,18 +3,15 @@ import LoVe.LoVelib
 
 theorem not_graded : LoVe.State = (String → Nat) := sorry
 
-/- 2 points -/
-theorem refl4 : 4 = 4 := sorry
+@[4 points] theorem refl4 : 4 = 4 := rfl
 
--- @[4 points] theorem refl4 : 4 = 4 := rfl
+@[7.25 points] theorem refl725 : 4 = 4 :=
+  rfl
 
--- @[7.25 points] theorem refl725 : 4 = 4 :=
---   rfl
+axiom t : 1 = 1
 
--- axiom t : 1 = 1
+@[1 point] theorem refl1 : 1 = 1 :=
+  t
 
--- @[1 point] theorem refl1 : 1 = 1 :=
---   t
-
--- @[1.0 point] theorem refl10 : 1.0 = 1.0 := sorry
+@[1.0 point] theorem refl10 : 1.0 = 1.0 := sorry
 
